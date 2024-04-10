7f4321cc655f7c63827b46ab4901d6a56df61019
Joseph Maxwell <joseph.maxwell@hyland.com>
2024-04-10 15:31:41 -0400

    actually use backend url in build

--- frontend/src/slices/apiSlice.ts
+++ frontend/src/slices/apiSlice.ts
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' });
+const baseQuery = fetchBaseQuery({ baseUrl: import.meta.env.VITE_BACKEND_URL });
 
 export const apiSlice = createApi({
   baseQuery,
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  console.log("backend url url: " + env.BACKEND_URL);
 
   return {
     plugins: [react()],
@@ -12,7 +11,7 @@ export default defineConfig(({ command, mode }) => {
       port: 3000,
       proxy: {
         '/api': {
-          target: env.BACKEND_URL,
+          target: env.VITE_BACKEND_URL,
           changeOrigin: true,
         },
       },
