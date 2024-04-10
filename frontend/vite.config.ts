8c66751f4726fe3a58d5fd577e72e61207a4a28a
Joseph Maxwell <joseph.maxwell@hyland.com>
2024-04-10 15:11:17 -0400

    test with log

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ command, mode }) => {
   return {
     plugins: [react()],
     server: {
-      port: 5000,
+      port: 3000,
       proxy: {
         '/api': {
           target: env.BACKEND_URL,
