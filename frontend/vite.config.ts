e36d687a22f8347fbc257cd4ebbf2c7b751dd8f7
Joseph Maxwell <joseph.maxwell@hyland.com>
2024-04-10 14:52:37 -0400

    test proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
+  console.log("backend url url: " + env.BACKEND_URL);
 
   return {
     plugins: [react()],
     server: {
-      port: 3000,
+      port: 5000,
       proxy: {
         '/api': {
           target: env.BACKEND_URL,
