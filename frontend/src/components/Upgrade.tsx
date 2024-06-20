bf6a1f4d7af9ddd7cfa5917ae03e8661e1bddbab
Leonardo Garman <leonardo.garman@hyland.com>
2024-06-20 09:30:28 -0400

    feat: Improve upgrade container with boxes and bg color

--- frontend/src/components/Upgrade.tsx
+++ frontend/src/components/Upgrade.tsx
@@ -4,15 +4,34 @@ interface IUpgradeProps {
   up: IUpgrade;
   status: UpgradeStatus;
   onBuy: (up: IUpgrade) => void;
+  currencyAmount: number;
 }
 
-const Upgrade = ({ up, status, onBuy }: IUpgradeProps) => {
+interface RGBTriple {
+  r: number;
+  g: number;
+  b: number;
+}
+const defUn = { r: 179, g: 176, b: 176 };
+const defAv = { r: 25, g: 224, b: 224 };
+
+const Upgrade = ({ up, status, onBuy, currencyAmount }: IUpgradeProps) => {
+  let bgColor = undefined;
+  if (status === "unavailable") {
+    const difference = { r: defUn.r - defAv.r, g: defUn.g - defAv.g, b: defUn.b - defAv.b };
+    let percentage = currencyAmount / up.cost;
+    bgColor = `rgb(${defUn.r - percentage * difference.r} ${defUn.g - percentage * difference.g} ${
+      defUn.b - percentage * difference.b
+    })`;
+  }
+
   return (
     <div
       className={`upgrade status-${status}`}
       onClick={() => {
         confirm("You sure?") && onBuy(up);
       }}
+      style={{ background: bgColor }}
     >
       <span style={{ fontWeight: "bold" }}>{up.name}</span> ({status})
     </div>
