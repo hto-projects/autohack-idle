944daedd569bbc2b6ef78ffa4e8ea047da20cd4a
Leonardo Garman <leonardo.garman@hyland.com>
2024-06-26 09:15:48 -0400

    refactor: Fix missing span in UpgradeImage

--- frontend/src/components/UpgradeImage.tsx
+++ frontend/src/components/UpgradeImage.tsx
@@ -5,24 +5,25 @@ interface IUpgradeImageProps {
 }
 
 export default function UpgradeImage({ picture }: IUpgradeImageProps) {
+  let imageElements: string = undefined;
   switch (picture.type) {
     case ImageType.string:
-      return picture.imageArr.join("");
+      imageElements = picture.imageArr.join("");
       break;
     case ImageType.png:
-      return (
-        <span className="upgrade-image">
-          {picture.imageArr.map((image) => (
-            <img
-              src={`public/assets/${image}.png`}
-              alt={image}
-              style={{ verticalAlign: "-17%", width: "21.97px", height: "21px", marginRight: "1%" }}
-            ></img>
-          ))}
-        </span>
-      );
+      imageElements = picture.imageArr
+        .map((image) => (
+          <img
+            src={`public/assets/${image}.png`}
+            alt={image}
+            style={{ verticalAlign: "-17%", width: "21.97px", height: "21px", marginRight: "1%" }}
+          ></img>
+        ))
+        .join("");
       break;
     default:
       console.log(`imageType: ${picture.type} is currently unsupported`);
   }
+
+  return <span className="upgrade-image">{imageElements}</span>;
 }
