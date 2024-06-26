da913b45952d4ae12be31ae8dd8ecdf897b6f413
Leonardo Garman <leonardo.garman@hyland.com>
2024-06-25 23:32:55 -0400

    feat: Update UpgradeImage component to render multiple emojis, pngs

--- frontend/src/components/UpgradeImage.tsx
+++ frontend/src/components/UpgradeImage.tsx
@@ -9,6 +9,25 @@ export default function UpgradeImage({ picture }: IUpgradeImageProps) {
     case ImageType.string:
       return picture.image;
       break;
+    case ImageType.png:
+      return (
+        // <img
+        //   src={`public/assets/${picture.image}.png`}
+        //   alt={picture.image}
+        //   style={{ verticalAlign: "-17%", width: "21.97px", height: "21px", marginRight: "1%" }}
+        // ></img>
+
+        <span className="upgrade-image">
+          {picture.image.map((image) => (
+            <img
+              src={`public/assets/${image}.png`}
+              alt={image}
+              style={{ verticalAlign: "-17%", width: "21.97px", height: "21px", marginRight: "1%" }}
+            ></img>
+          ))}
+        </span>
+      );
+      break;
     default:
       console.log(`imageType: ${picture.type} is currently unsupported`);
   }
