1631a00dc94fbc956c940d13b4b28ae296ac4ef0
Leonardo Garman <leonardo.garman@hyland.com>
2024-06-27 14:09:31 -0400

    feat: Update DesktopAppImage component

--- frontend/src/components/Image.tsx
+++ frontend/src/components/Image.tsx
@@ -1,43 +1,54 @@
 import { ImageType, IAppImage } from "../../../shared/types";
 
+// imgStyle's type should be an interface for the style properties of an <img> tag but I can't find it
 interface ImageProps {
+  className: any;
   picture: IAppImage;
-  imgTagStyle: any;
+  imgStyle?: any;
 }
 
-interface ImagePropsFoo {
+interface ImageWrapperProps {
   picture: IAppImage;
 }
 
-export function Image({ picture, imgTagStyle }: ImageProps) {
+export function Image({ className, picture, imgStyle }: ImageProps) {
   // imageElements is currently type "any" because TS had issues assigning type "Element[]" to the type "string | Element[]"
-  let imageElements: any = undefined;
+  let imageElements: any = <></>;
   switch (picture.type) {
     case ImageType.string:
       imageElements = picture.image;
       break;
     case ImageType.png:
       imageElements = (
-        <img src={`public/assets/${picture.image}.png`} alt={picture.image.replace("/", "_")} style={imgTagStyle}></img>
+        <img src={`public/assets/${picture.image}.png`} alt={picture.image.replace("/", "_")} style={imgStyle}></img>
       );
       break;
     default:
       console.log(`imageType: ${picture.type} is currently unsupported`);
   }
 
-  return imageElements;
+  return <span className={className}>{imageElements}</span>;
 }
 
-export function UpgradeImage({ picture }: ImagePropsFoo) {
-  let styleObj = { verticalAlign: "-17%", width: "21.97px", height: "21px", marginRight: "1%" };
-
+export function UpgradeImage({ picture }: ImageWrapperProps) {
   return (
-    <span className="upgrade-image">
-      <Image picture={picture} imgTagStyle={styleObj}></Image>
-    </span>
+    <Image
+      className={"upgrade-image"}
+      picture={picture}
+      imgStyle={{ verticalAlign: "-17%", width: "21.97px", height: "21px", marginRight: "1%" }}
+    ></Image>
   );
 }
 
-export function DesktopImage({ picture }: ImagePropsFoo) {
-  return <span className="desktop-image">{<Image picture={picture}></Image>}</span>;
+export function DesktopAppImage({ picture }: ImageWrapperProps) {
+  let updatedPicture = structuredClone(picture);
+  updatedPicture.image = `app_icons/${picture.image}`;
+
+  return (
+    <Image
+      className={"desktop-image"}
+      picture={updatedPicture}
+      imgStyle={{ width: "128px", height: "128px", imageRendering: "pixelated" }}
+    ></Image>
+  );
 }
