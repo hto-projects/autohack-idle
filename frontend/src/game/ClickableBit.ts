ee03bf6782909359e3a1d61718f89afe77a8ba10
Leonardo Garman <leonardo.garman@hyland.com>
2024-07-03 15:38:30 -0400

    feat: Update all

--- frontend/src/game/ClickableBit.ts
+++ frontend/src/game/ClickableBit.ts
@@ -1,16 +1,60 @@
 import { GameObjects, Scene } from "phaser";
 import { EventBus } from "./EventBus";
 
-export default class ClickableBit extends GameObjects.BitmapText {
+export default class ClickableBit extends GameObjects.Text {
+  static arr: Array<ClickableBit>;
+  static charset: Array<string> = ["0", "1"];
+  startheight: number;
+  charsetI: number = 0;
+  toDestroy: boolean;
+
   constructor(scene: Scene, x: number, y: number) {
-    super(scene, x, y, "arcade", "1");
+    super(scene, x, y, "1", {});
     this.setInteractive();
     this.on("pointerdown", () => {
       EventBus.emit("add-bit");
-      this.destroy();
+      // this.destroyObj();
+      this.toDestroy = true;
     });
 
     scene.add.existing(this);
     scene.physics.add.existing(this);
+
+    this.startheight = 0;
+    // ClickableBit.charset = ["0", "1"];
+    this.init(); // this.changeText();
+  }
+
+  init() {
+    this.scene.tweens.add({
+      targets: this,
+      y: { from: this.startheight, to: this.startheight + 1000 },
+      duration: 5000,
+      onComplete: () => {
+        // ClickableBit.array = ClickableBit.array.filter((elem) => elem !== this);
+        // this.destroyObj();
+        this.toDestroy = true;
+        // console.log(ClickableBit.arr);
+      }
+    });
+  }
+
+  changeText() {
+    if (this.charsetI === ClickableBit.charset.length - 1) {
+      this.charsetI = 0;
+    } else {
+      this.charsetI++;
+    }
+    console.log("change");
+    this.setText(ClickableBit.charset[this.charsetI]);
+  }
+
+  destroyObj() {
+    if (this) {
+      console.log("delete");
+      ClickableBit.arr.splice(ClickableBit.arr.indexOf(this), 1);
+      this.destroy();
+      // console.log(ClickableBit.array);
+    }
   }
 }
--- frontend/src/game/scenes/Collect2.ts
+++ frontend/src/game/scenes/Collect2.ts
@@ -6,12 +6,14 @@ import { calculateVariableValue, pluckOne } from "../../../../shared/util";
 
 export class Collect2 extends Scene {
   numBitsText: GameObjects.Text;
-  // clickyBits: Array<ClickableBit>;
+  clickyBits: Array<ClickableBit> = [];
   bitAppearEvent: any;
+  accrewedTime: number = 0;
 
   constructor() {
     super("Collect2");
   }
+
   maybeAddBit(bitAppearanceProb) {
     if (Math.random() < bitAppearanceProb) {
       this.addNewBit();
@@ -30,35 +32,23 @@ export class Collect2 extends Scene {
 
   addNewBit() {
     const { xPos, yPos } = this.getRandomBitPosition();
-    new ClickableBit(this, xPos, yPos);
+    const newBit = new ClickableBit(this, xPos, yPos);
+    this.clickyBits.push(newBit);
+    console.log("created");
   }
+
   create() {
     const screenTopY = this.cameras.main.worldView.y;
-    const screenBottomY = screenTopY + this.cameras.main.height;
+    // const screenBottomY = screenTopY + this.cameras.main.height;
     const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
     const dataFontSize = 16;
-
     this.cameras.main.setBackgroundColor("#100000");
-
-    this.numBitsText = this.add
-      .text(screenCenterX, screenTopY + dataFontSize, `hm`, {
-        fontFamily: "consolas",
-        fontSize: dataFontSize,
-        color: "#ffffff",
-        align: "center"
-      })
-      .setOrigin(0.5)
-      .setDepth(100);
-
-    EventBus.on("change-bits", (bits: number) => {
-      this.numBitsText.setText(`aata: ${bits} bits`);
-    });
+    ClickableBit.arr = this.clickyBits;
 
     EventBus.on("change-rates", (data) => {
       if (this.bitAppearEvent) {
         this.time.removeEvent(this.bitAppearEvent);
       }
-
       this.bitAppearEvent = this.time.addEvent({
         callback: this.maybeAddBit,
         args: [data.bitAppearanceProbability],
@@ -86,7 +76,26 @@ export class Collect2 extends Scene {
     });
 
     EventBus.emit("current-scene-ready", this);
+    this.time.delayedCall(5000, () => console.log(this.clickyBits));
+    this.time.delayedCall(10000, () => console.log(this.clickyBits));
+    this.time.delayedCall(15000, () => console.log(this.clickyBits));
+    // ClickableBit.charset = ["a", "b"];
   }
 
-  update() {}
+  update(time: number, delta: number) {
+    const changeTextTime = 500;
+    this.accrewedTime += delta;
+    if (this.accrewedTime > changeTextTime) {
+      for (let bit of this.clickyBits) {
+        bit.changeText();
+      }
+      this.accrewedTime = 0;
+
+      for (let bit of this.clickyBits) {
+        if (bit.toDestroy) {
+          bit.destroyObj();
+        }
+      }
+    }
+  }
 }
