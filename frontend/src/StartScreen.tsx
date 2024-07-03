8ce4275ccbb1c04c766a09a9e064877f8041fc89
Jude Klatte <jude.klatte@icloud.com>
2024-07-03 14:30:35 -0400

    Fixed a Bug and Added a Quick Fix

--- frontend/src/StartScreen.tsx
+++ frontend/src/StartScreen.tsx
@@ -1,10 +1,12 @@
 export default function StartScreen({ setStartShown }) {
   return (
-    <div id="start-screen" style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
+    <div id="start-screen" style={{ height: "100vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
       <p>C:/AUTOHACK/IDLE</p>
+      <div id="space"></div>
       <button id="playButton" onClick={() => setStartShown(false)}>
         Play
       </button>
+      <div id="space2"></div>
     </div>
   );
 }
