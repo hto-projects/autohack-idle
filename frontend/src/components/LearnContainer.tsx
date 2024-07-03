ab3a1a8dc7eb93391fd52531746a4de5b0862081
Joseph Maxwell <joseph.maxwell@hyland.com>
2024-07-03 15:58:41 -0400

    add syntax highlighter example

--- frontend/src/components/LearnContainer.tsx
+++ frontend/src/components/LearnContainer.tsx
@@ -1,7 +1,19 @@
+import SyntaxHighlighter from "react-syntax-highlighter";
+
 export default function LearnContainer({ children }) {
+  const jsCode = `function something() {
+  // this is a comment
+  alert("Hi!");
+  console.log("Hello, world!");
+  let y = 4;  
+}
+`;
+
   return (
     <div className="LearnContainer">
       <h2> Learn feature coming soon!</h2>
+
+      <SyntaxHighlighter language="javascript">{jsCode}</SyntaxHighlighter>
     </div>
   );
 }
