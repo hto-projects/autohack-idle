8b256a1e068d0e22ffd7ef5890ad9c935868df76
Jude Klatte <jude.klatte@icloud.com>
2024-07-03 14:32:12 -0400

    Removed Unused Import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,6 @@ import store from "./store.js";
 import { Provider } from "react-redux";
 import Profile from "./components/Auth/Profile.js";
 import PrivateRoute from "./components/Auth/PrivateRoute.js";
-import StartScreen from "./StartScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
