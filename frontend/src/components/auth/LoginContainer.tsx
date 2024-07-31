209ea6c3bc8d8e4db84e936ba6c0e12fca80b006
Emad Sumrain <sumrain.emad23@gmail.com>
2024-07-31 09:48:40 -0400

    add in the new learn and help stuff

---
+++ frontend/src/components/auth/LoginContainer.tsx
@@ -0,0 +1,33 @@
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLogoutMutation } from "../../slices/usersApiSlice";
+import { logout } from "../../slices/authSlice";
+import MainAuth from "./MainAuth";
+import { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
+
+export default function LoginContainer() {
+  const { userInfo } = useSelector((state: any) => state.auth);
+  const [screenShowing, setScreenShowing] = useState<string>();
+
+  let screenComponent;
+
+  switch (screenShowing) {
+    case "login":
+      screenComponent = <Login setScreen={setScreenShowing}></Login>;
+      break;
+    case "register":
+      screenComponent = <Register setScreen={setScreenShowing}></Register>;
+      break;
+    default:
+      screenComponent = <MainAuth userInfo={userInfo} setScreen={setScreenShowing}></MainAuth>;
+  }
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  return <div style={{ textAlign: "center" }}>{screenComponent}</div>;
+}
