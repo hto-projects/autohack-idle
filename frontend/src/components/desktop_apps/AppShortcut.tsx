177370d3f40f802e4f80d34e2c7779758c72111f
Leonardo Garman <leonardo.garman@hyland.com>
2024-08-01 11:13:44 -0400

    style: Formal AppShortcut

--- frontend/src/components/desktop_apps/AppShortcut.tsx
+++ frontend/src/components/desktop_apps/AppShortcut.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { AppType } from "../../../../shared/types";
 import { useSelector } from "react-redux";
 import { IGameState } from "../../store";
-import { wrap } from "module";
 
 interface AppShortcutProps {
   appType: AppType;
@@ -11,13 +9,13 @@ interface AppShortcutProps {
   useSmaller?: boolean;
 }
 
-const AppShortcut: React.FC<AppShortcutProps> = ({ appType, setOpen, icon, useSmaller = false }) => {
+export default function AppShortcut({ appType, setOpen, icon, useSmaller = false }: AppShortcutProps) {
+  const appTextColor: string = useSelector((state: IGameState) => state.styleData.textColor.app);
+  const appTextFont: string = useSelector((state: IGameState) => state.styleData.textFont.app);
+
   const shownIcon = `url(assets/app_icons/${icon ?? `${appType.toLowerCase()}`}.png)`;
   const size = useSmaller ? "64px" : "120px";
-  const appTextColor: string = useSelector((state: IGameState) => state.styleData.textColor.app);
   const appTextSize = useSmaller ? "18px" : "20px";
-  console.log(appTextSize);
-  const appTextFont: string = useSelector((state: IGameState) => state.styleData.textFont.app);
 
   return (
     <div
@@ -48,6 +46,4 @@ const AppShortcut: React.FC<AppShortcutProps> = ({ appType, setOpen, icon, useSm
       {appType}
     </div>
   );
-};
-
-export default AppShortcut;
+}
