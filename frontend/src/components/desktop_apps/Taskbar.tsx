f90ecf4be650d0886f8266b60bc63c547e7f6105
Leonardo Garman <leonardo.garman@hyland.com>
2024-08-06 10:39:37 -0400

    style: Tidy up Taskbar

--- frontend/src/components/desktop_apps/Taskbar.tsx
+++ frontend/src/components/desktop_apps/Taskbar.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import { IGameData } from "../../../../shared/types";
 import { useSelector } from "react-redux";
 import { AppType } from "../../../../shared/types";
 import AutoCollector from "../AutoCollector";
 import AppWindow from "./AppWindow";
 import AppShortcut from "./AppShortcut";
 import { IGameState } from "../../store";
+import { IGameData } from "../../slices/gameDataSlice";
 
 const Taskbar: React.FC = () => {
-  const gameData: IGameData = useSelector((state: any) => state.gameData);
   const [openWindow, setOpenWindow] = React.useState(null as AppType | null);
   const display = openWindow === null ? null : <AppWindow open={openWindow} setOpen={setOpenWindow}></AppWindow>;
+
+  const gameData: IGameData = useSelector((state: any) => state.gameData);
   const userInfo = useSelector((state: IGameState) => state.auth.userInfo);
   const taskBarColor: string = useSelector((state: IGameState) => state.styleData.backgroundColor.taskbar);
   const taskBarTextColor: string = useSelector((state: IGameState) => state.styleData.textColor.taskbar);
   const taskBarTextSize: string = useSelector((state: IGameState) => state.styleData.textSize.taskbar);
-  const taskBarTextFont: string = useSelector((state: IGameState) => state.styleData.textFont.taskbar);
+
   return (
     <div
       id="taskbar"
