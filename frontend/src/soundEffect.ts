e61111f9c8ecc47cdabffa010bfc5d89bbf953c2
Jacob Gusching <Jacob.Gusching@hyland.com>
2024-08-08 13:04:51 -0400

    Remove "public" from asset directory location

--- frontend/src/soundEffect.ts
+++ frontend/src/soundEffect.ts
@@ -1,12 +1,12 @@
 const soundEffects = {
-  al: new Audio("../public/assets/audio/sound_effects/al.wav"),
-  bitCollect: new Audio("../public/assets/audio/sound_effects/bitCollect.wav"),
-  close: new Audio("../public/assets/audio/sound_effects/close.wav"),
-  deepClick: new Audio("../public/assets/audio/sound_effects/deepClick.wav"),
-  explosion: new Audio("../public/assets/audio/sound_effects/explosion.wav"),
-  open: new Audio("../public/assets/audio/sound_effects/open.wav"),
-  shady: new Audio("../public/assets/audio/sound_effects/shady.wav"),
-  trustworthy: new Audio("../public/assets/audio/sound_effects/trustworthy.wav")
+  al: new Audio("../assets/audio/sound_effects/al.wav"),
+  bitCollect: new Audio("../assets/audio/sound_effects/bitCollect.wav"),
+  close: new Audio("../assets/audio/sound_effects/close.wav"),
+  deepClick: new Audio("../assets/audio/sound_effects/deepClick.wav"),
+  explosion: new Audio("../assets/audio/sound_effects/explosion.wav"),
+  open: new Audio("../assets/audio/sound_effects/open.wav"),
+  shady: new Audio("../assets/audio/sound_effects/shady.wav"),
+  trustworthy: new Audio("../assets/audio/sound_effects/trustworthy.wav")
 };
 
 export function playSoundEffect(name: string) {
