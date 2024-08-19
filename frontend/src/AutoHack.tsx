5f4f0730eba39d42e45ec01b564da14511d146f3
Leonardo Garman <leonardo.garman@hyland.com>
2024-08-19 10:21:11 -0400

    fix: Rem multiple displays

--- frontend/src/AutoHack.tsx
+++ frontend/src/AutoHack.tsx
@@ -4,9 +4,12 @@ import Desktop from "./components/desktop_apps/Desktop";
 import Taskbar from "./components/desktop_apps/Taskbar";
 import HorizontalBar from "./components/desktop_apps/screens/HorizontalBar";
 import VerticalBar from "./components/desktop_apps/screens/VerticalBar";
+import { AppType } from "../../shared/types";
+import React from "react";
 
 export default function AutoHack() {
   const [startShown, setStartShown] = useState<boolean>(true);
+  const [openWindow, setOpenWindow] = React.useState<AppType>(null);
 
   if (startShown === true) {
     return <StartScreen setStartShown={setStartShown}></StartScreen>;
@@ -18,10 +21,10 @@ export default function AutoHack() {
       <div id="auto-hack" style={{ display: "flex", flex: "96%", flexDirection: "column" }}>
         <HorizontalBar></HorizontalBar>
         <div style={{ flex: "95%" }}>
-          <Desktop></Desktop>
+          <Desktop openWindow={openWindow} setOpenWindow={setOpenWindow}></Desktop>
         </div>
         <div style={{ flex: "5%" }}>
-          <Taskbar></Taskbar>
+          <Taskbar setOpenWindow={setOpenWindow}></Taskbar>
         </div>
         <HorizontalBar></HorizontalBar>
       </div>
