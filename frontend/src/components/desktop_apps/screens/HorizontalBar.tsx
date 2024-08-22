1ddaaf149f0490f152dcfb1dbf9849c536d2528f
Leonardo Garman <leonardo.garman@hyland.com>
2024-08-22 13:10:17 -0400

    style: Delete unused, moved files

--- frontend/src/components/desktop_apps/screens/HorizontalBar.tsx
+++
@@ -1,7 +0,0 @@
-import { useSelector } from "react-redux";
-import { IGameState } from "../../../store";
-
-export default function HorizontalBar() {
-  const horizontalBarColor: string = useSelector((state: IGameState) => state.styleData.backgroundColor.horizontalbar);
-  return <div style={{ width: "100%", flex: "3%", backgroundColor: horizontalBarColor }}></div>;
-}
--- frontend/src/components/desktop_apps/screens/VerticalBar.tsx
+++
@@ -1,7 +0,0 @@
-import { useSelector } from "react-redux";
-import { IGameState } from "../../../store";
-
-export default function VerticalBar() {
-  const verticalBarColor: string = useSelector((state: IGameState) => state.styleData.backgroundColor.verticalbar);
-  return <div style={{ height: "100%", flex: "2%", backgroundColor: verticalBarColor }}></div>;
-}
